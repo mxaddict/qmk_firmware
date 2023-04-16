2733ef4d8f92958f2371cb839cfc876ab05e7da8
mxaddict <mxaddict@codedmaster.com>
2023-04-16 10:05:26 +0800

    Create new mapping for choco fi...

---
+++ keyboards/crkbd/keymaps/mxaddict/rules.mk
@@ -0,0 +1,13 @@
+# Make the binary smaller
+LTO_ENABLE      = yes
+
+# Enable Oled support
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+
+# Enable words per minute
+WPM_ENABLE      = yes
+
+# Disable mousekey and RGB
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no
